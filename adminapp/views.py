ca11664e790a94aa705567800516637e9912ee15
Sergey Krivoruchko <tigra2004@gmail.com>
2022-01-18 12:51:08 +0700

    lesson_7. CRUD users

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,15 +1,21 @@
 from django.conf import settings
-from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, redirect, render
+from django.urls import reverse
 
+from adminapp.forms import ShopUserAdminEditForm
+from authnapp.forms import ShopUserRegisterForm
 from authnapp.models import ShopUser
 from mainapp.models import Product, ProductCategory
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_main(request):
     response = redirect("admin:users")
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = "админка/пользователи"
     users_list = ShopUser.objects.all().order_by("-is_active", "-is_superuser", "-is_staff", "username")
@@ -18,20 +24,56 @@ def users(request):
 
 
 def user_create(request):
-    response = redirect("admin:users")
-    return response
+    title = "пользователи/создание"
+
+    if request.method == "POST":
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse("admin:users"))
+    else:
+        user_form = ShopUserRegisterForm()
+
+    content = {"title": title, "update_form": user_form, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_update.html", content)
 
 
 def user_update(request, pk):
-    response = redirect("admin:users")
-    return response
+    title = "пользователи/редактирование"
+
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == "POST":
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse("admin:user_update", args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {"title": title, "update_form": edit_form, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_update.html", content)
 
 
 def user_delete(request, pk):
-    response = redirect("admin:users")
-    return response
+    title = "пользователи/удаление"
+
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == "POST":
+        # user.delete()
+        # Instead delete we will set users inactive
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse("admin:users"))
+
+    content = {"title": title, "user_to_delete": user, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_delete.html", content)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def categories(request):
     title = "админка/категории"
     categories_list = ProductCategory.objects.all()
@@ -54,6 +96,7 @@ def category_delete(request, pk):
     return response
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def products(request, pk):
     title = "админка/продукт"
     category = get_object_or_404(ProductCategory, pk=pk)
